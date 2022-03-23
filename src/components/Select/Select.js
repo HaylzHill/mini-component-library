526a8960f8a01da2c738847e27b6598ef154a47b
Hayley <hayleyhill.x@live.co.uk>
2022-03-23 23:26:13 +0000

    Add select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,57 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  appearance: none;
+`;
+
+const DropDown = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  border-radius: 8px;
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+  display: flex;
+  align-items: center;
+
+  ${NativeSelect}:focus + & {
+    outline: 2px solid black;
+    outline: 2px auto -webkit-focus-ring-colour;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const Value = styled.p`
+  margin-right: 24px;
+`;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <DropDown>
+        <Value>{displayedValue}</Value>
+        <Icon id="chevron-down" stroke-width={1} size="20px"></Icon>
+      </DropDown>
+    </Wrapper>
   );
 };
 
